f3180dbb6b83f035100bf274e5ba01df19a43d50
349979815-design <349979815@gapps.yrdsb.ca>
2026-05-04 11:30:13 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,178 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . # #
+        . . . # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # # #
+        . . . # #
+        . . . # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # # #
+        . . # # #
+        . . . # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # # #
+        . # # # #
+        . . . # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . # # # #
+        . # # # #
+        . . . # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . # # # #
+        . . . # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # . . # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # . # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        . . # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showString("If u can see this super amazing thingy ma bob just know that abdel is a super sigma alpha male with 67 billion aura and amir is a lame boomer")
+})
